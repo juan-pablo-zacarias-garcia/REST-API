bbb96941b9483af217508f4cc2dd53187ca30a79
juan-pablo-zacarias-garcia <zacarias.garcia.juanpablo.1j@gmail.com>
2021-01-17 16:44:43 -0600

    Rutas-y-usuarios

---
+++ rest-api/app/Controllers/Http/UserController.js
@@ -0,0 +1,27 @@
+'use strict'
+
+//Importar el modelo de usuario
+const User = use('App/Models/User')
+
+class UserController {
+    //Metodo para el logging
+    async login({ request, auth }) {
+        const { email, password } = request.all();
+        const token = await auth.attempt(email, password);
+        return token;
+    }
+
+    async store({ request }) {
+        const { email, password } = request.all();
+        console.log(email, password);
+        const user = await User.create({
+            email,
+            password,
+            username: email
+        });
+        return this.login(...arguments);
+
+    };
+}
+
+module.exports = UserController
--- rest-api/start/routes.js
+++ rest-api/start/routes.js
@@ -16,6 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-    return { greeting: 'Hello world in JSON' }
-})
+Route.group(() => {
+    Route.post('usuarios/registro', 'UserController.store');
+    Route.post('usuarios/login', 'UserController.login');
+}).prefix('api/v1/');
