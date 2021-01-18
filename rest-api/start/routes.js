8ed1f691c3b493c68a760387af1d51d82158b442
juan-pablo-zacarias-garcia <zacarias.garcia.juanpablo.1j@gmail.com>
2021-01-18 14:10:26 -0600

    Método create

--- rest-api/start/routes.js
+++ rest-api/start/routes.js
@@ -19,5 +19,7 @@ const Route = use('Route')
 Route.group(() => {
     Route.post('usuarios/registro', 'UserController.store');
     Route.post('usuarios/login', 'UserController.login');
-    Route.get('proyectos', 'ProyectoController.index');
+    Route.get('proyectos', 'ProyectoController.index').middleware('auth');
+    Route.post('proyectos', 'ProyectoController.create').middleware('auth');
+
 }).prefix('api/v1/');
