83bdd04ef10a64ddd6ab98865a8215601f9896b2
juan-pablo-zacarias-garcia <zacarias.garcia.juanpablo.1j@gmail.com>
2021-01-18 18:03:30 -0600

    Método Update
    Método par aactualizar el nombre de un proyecto
    
    Co-Authored-By: Joshmk8 <50975696+Joshmk8@users.noreply.github.com>

--- rest-api/app/Controllers/Http/ProyectoController.js
+++ rest-api/app/Controllers/Http/ProyectoController.js
@@ -29,6 +29,16 @@ class ProyectoController {
         await proyecto.delete();
         return proyecto;
     }
+
+    async update({ auth, params, request }) {
+        const user = await auth.getUser();
+        const { id } = params;
+        const proyecto = await Proyecto.find(id);
+        AutorizacionService.verificarPermiso(proyecto, user);
+        proyecto.merge(request.only('nombre'));
+        await proyecto.save();
+        return proyecto;
+    }
 }
 
 module.exports = ProyectoController
